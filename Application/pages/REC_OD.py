8501a7a4f96c703b39b7f62747304f73aab6a155
Alfred Sam <alfredsam2006@gmail.com>
2025-03-05 20:24:10 +0530

    Update REC_OD.py

--- Application/pages/REC_OD.py
+++ Application/pages/REC_OD.py
@@ -12,8 +12,8 @@ from docx.enum.section import WD_ORIENT
 if not st.session_state.get("authenticated", False):
     # login()
     # st.stop()
-    url = "https://intellexa-techlead.streamlit.app/"  # Replace with your desired URL
-    st.markdown(f'<meta http-equiv="refresh" content="0;url={url}">', unsafe_allow_html=True)
+    # url = "https://intellexa-techlead.streamlit.app/"  # Replace with your desired URL
+    # st.markdown(f'<meta http-equiv="refresh" content="0;url={url}">', unsafe_allow_html=True)
     st.stop()
 
 def load_csv_file():
