3d0bdc48c4f023a4b2807f94e7a745254aef70e9
Alfred Sam <alfredsam2006@gmail.com>
2025-03-05 20:03:59 +0530

    Update Intellexa_Login.py

--- Application/Intellexa_Login.py
+++ Application/Intellexa_Login.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-USERNAME = st.secrets["username"]
-PASSWORD = st.secrets["password"]
+
 
 # Initialize session state
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
 
 def login():
+    USERNAME = st.secrets["username"]
+    PASSWORD = st.secrets["password"]
     """Simple authentication function."""
     st.markdown("<h2 style='text-align: center;'>🔒 Login to Intellexa</h2>", unsafe_allow_html=True)
     st.markdown("""
